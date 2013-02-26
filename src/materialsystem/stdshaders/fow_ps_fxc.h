4857014851137e328a31cf6415e5f8c1378c048e
Sandern <vannoortsander@gmail.com>
2013-02-26 20:37:00 +0100

    Added option for Swarm Keeper to compile the shaders with the unexplored fog of war being made full black.

--- src/materialsystem/stdshaders/fow_ps_fxc.h
+++ src/materialsystem/stdshaders/fow_ps_fxc.h
@@ -20,15 +20,28 @@ static const float3 g_FoWToGray		= float3( 0.3, 0.59, 0.11 );
 #define FOW_COLOR_FACTOR	0.25
 #define FOW_GRAY_HILIGHTS	2.0
 
+// Make explored full dark, instead of darker
+#define EXPLORED_FULLDARK 0 
+
 float3 CalcFoW( const sampler FoWSampler, const float2 vFoWCoords, const float3 CurrentColor )
 {
 	float	vFoWResult = tex2D( FoWSampler, vFoWCoords.xy ).r;
 
 	float3	vGray = pow( dot( CurrentColor, g_FoWToGray ), FOW_GRAY_HILIGHTS );
+#ifndef EXPLORED_FULLDARK
 	float fDarknessValue = FOW_DARKNESS_VALUE - (0.35 - (0.35 * tex2D( FoWSampler, vFoWCoords.xy ).g));
 	float3	vFinalColor = ( vGray * FOW_GRAY_FACTOR	 * fDarknessValue ) + ( CurrentColor * FOW_COLOR_FACTOR * fDarknessValue );
-
 	return ( vFinalColor * ( 1.0 - vFoWResult ) ) + ( CurrentColor * vFoWResult );
+#else
+	float3	vFinalColor = ( vGray * FOW_GRAY_FACTOR	 * FOW_DARKNESS_VALUE ) + ( CurrentColor * FOW_COLOR_FACTOR * FOW_DARKNESS_VALUE );
+	float3 vResult = ( vFinalColor * ( 1.0 - vFoWResult ) ) + ( CurrentColor * vFoWResult );
+
+	float fExplored = tex2D( FoWSampler, vFoWCoords.xy ).g;
+	vResult = (vResult * fExplored ) + ( float3(0,0,0) * ( 1.0f - fExplored ) );
+	return vResult;
+#endif // EXPLORED_FULLDARK
+
+
 }
 
 
