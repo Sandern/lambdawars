7ea9ed4398b9d172d64b669682097af595ccb64a
Sandern <vannoortsander@gmail.com>
2013-03-30 18:52:55 +0100

    Updated tile base entity for swarm keeper (not used yet).

--- src/game/shared/keeper/tilebase.cpp
+++ src/game/shared/keeper/tilebase.cpp
@@ -17,7 +17,9 @@
 //-----------------------------------------------------------------------------
 CTileBase::CTileBase( void )
 {
+#ifndef CLIENT_DLL
 	SetDensityMapType( DENSITY_NONE );
+#endif // CLIENT_DLL
 }
 
 //-----------------------------------------------------------------------------
@@ -27,7 +29,22 @@ void CTileBase::Spawn( void )
 {
 	BaseClass::Spawn();
 
+    SetSolid( SOLID_BBOX );
+    SetMoveType( MOVETYPE_NONE );
 
+#ifndef CLIENT_DLL
+	AddFlag( FL_STATICPROP );
+#else
+	InvalidatePhysicsRecursive( POSITION_CHANGED | ANGLES_CHANGED | VELOCITY_CHANGED );
+    UpdatePartitionListEntry();
+    UpdateVisibility();
+	// CreateShadow() // TODO: Seems bugged
+             
+    // Reset interpolation history, so it doesn't contains shit
+    // -> shouldn't do anything for client side created entities...
+    //    Apparently it does do something, since it fucks up the angles of all tiles.
+    // ResetLatched() 
+#endif // CLIENT_DLL
 }
 
 //-----------------------------------------------------------------------------
