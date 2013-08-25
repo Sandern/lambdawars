0ad08be598c36dd13449b33abaac46f7ba80dbdb
Sandern <vannoortsander@gmail.com>
2013-08-25 12:37:22 +0200

    Cleaned up hl2wars_client.cpp

--- src/game/server/hl2wars/hl2wars_client.cpp
+++ src/game/server/hl2wars/hl2wars_client.cpp
@@ -89,14 +89,6 @@ void ClientPutInServer( edict_t *pEdict, const char *playername )
 		Warning("ClientPutInServer: Failed to spawn entity %s. Falling back to player.\n", pClassname);
 	}
 	pPlayer->SetPlayerName( playername );
-
-	/*
-	INetChannelInfo *nci = engine->GetPlayerNetInfo(pPlayer->entindex());
-	INetChannel *nc = (INetChannel *)nci;
-	Msg("Buffer size before: %d\n", nc->GetBufferSize());
-	nc->SetMaxBufferSize(true, 1000000000);
-	Msg("Buffer size after: %d\n", nc->GetBufferSize());
-	*/
 }
 
 void ClientActive( edict_t *pEdict, bool bLoadGame )
@@ -108,22 +100,22 @@ void ClientActive( edict_t *pEdict, bool bLoadGame )
 	FinishClientPutInServer( pPlayer );
 
 #ifdef ENABLE_PYTHON
-	// Give a full update of the networked python entities
-	// NOTE: Only dedicated servers and the listened host. Listened and clients are done in ClientConnect
-	if( engine->IsDedicatedServer() || ENTINDEX(pEdict) > 1 )
-	{
-		FullClientUpdatePyNetworkCls( pPlayer );
-	}
-
-#ifdef CLIENT_DLL
-	char pLevelName[_MAX_PATH];
-	Q_FileBase(engine->GetLevelName(), pLevelName, _MAX_PATH);
-#else
-	const char *pLevelName = STRING(gpGlobals->mapname);
-#endif
-
 	if( SrcPySystem()->IsPythonRunning() )
 	{
+		// Give a full update of the networked python entities
+		// NOTE: Only dedicated servers and the listened host. Listened and clients are done in ClientConnect
+		if( engine->IsDedicatedServer() || ENTINDEX(pEdict) > 1 )
+		{
+			FullClientUpdatePyNetworkCls( pPlayer );
+		}
+
+#ifdef CLIENT_DLL
+		char pLevelName[_MAX_PATH];
+		V_FileBase(engine->GetLevelName(), pLevelName, _MAX_PATH);
+#else
+		const char *pLevelName = STRING(gpGlobals->mapname);
+#endif
+
 		// Notify gamemgr (before other client active's)
 		boost::python::object ca;
 		try {
@@ -135,19 +127,20 @@ void ClientActive( edict_t *pEdict, bool bLoadGame )
 		}
 
 		// Send clientactive signal
-		try {
+		try 
+		{
 			bp::dict kwargs;
 			kwargs["sender"] = bp::object();
 			kwargs["client"] = pPlayer->GetPyHandle();
-			bp::object signal = SrcPySystem()->Get("clientactive", "core.signals", true);
+			bp::object signal = SrcPySystem()->Get( "clientactive", "core.signals", true );
 			SrcPySystem()->CallSignal( signal, kwargs );
-			//SrcPySystem()->SignalCheckResponses( PyEval_CallObjectWithKeywords( method.ptr(), NULL, kwargs.ptr() ) );
 
-			signal = SrcPySystem()->Get("map_clientactive", "core.signals", true)[pLevelName];
+			signal = SrcPySystem()->Get( "map_clientactive", "core.signals", true )[pLevelName];
 			SrcPySystem()->CallSignal( signal, kwargs );
-			//SrcPySystem()->SignalCheckResponses( PyEval_CallObjectWithKeywords( method.ptr(), NULL, kwargs.ptr() ) );
-		} catch( bp::error_already_set & ) {
-			Warning("Failed to retrieve clientactive signal:\n");
+		} 
+		catch( bp::error_already_set & ) 
+		{
+			Warning( "Failed to retrieve clientactive signal:\n" );
 			PyErr_Print();
 		}
 	}
