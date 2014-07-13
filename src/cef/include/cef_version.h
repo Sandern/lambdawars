0609d520488bddcf5611ec482b584e7956c0dfba
Sandern <vannoortsander@gmail.com>
2014-07-13 19:22:28 +0200

    Update Chromium Embedded Framework to revision 1768 (chromium version 33.0.1750.170).

--- src/cef/include/cef_version.h
+++ src/cef/include/cef_version.h
@@ -36,13 +36,13 @@
 #define CEF_INCLUDE_CEF_VERSION_H_
 
 #define CEF_VERSION_MAJOR 3
-#define CEF_REVISION 1629
+#define CEF_REVISION 1768
 #define COPYRIGHT_YEAR 2014
 
 #define CHROME_VERSION_MAJOR 33
 #define CHROME_VERSION_MINOR 0
 #define CHROME_VERSION_BUILD 1750
-#define CHROME_VERSION_PATCH 117
+#define CHROME_VERSION_PATCH 170
 
 #define DO_MAKE_STRING(p) #p
 #define MAKE_STRING(p) DO_MAKE_STRING(p)
@@ -61,13 +61,13 @@ extern "C" {
 // universal hash value will change if any platform is affected whereas the
 // platform hash values will change only if that particular platform is
 // affected.
-#define CEF_API_HASH_UNIVERSAL "e9667f2272f1ae42985a27dfa82fcee0b42da587"
+#define CEF_API_HASH_UNIVERSAL "97238e85785c70a4f1c5e3eec6f67fcbef2df89e"
 #if defined(OS_WIN)
-#define CEF_API_HASH_PLATFORM "fcdffdfb7ef63f8a88d18623b49981fe62ec6d75"
+#define CEF_API_HASH_PLATFORM "2a84f1539871434274323d2d6742545820a42dfe"
 #elif defined(OS_MACOSX)
-#define CEF_API_HASH_PLATFORM "623ddebdfbfb44fe31420943ea4495a9dbd08323"
+#define CEF_API_HASH_PLATFORM "7b1d9369d55ee8a12a7a848e3b82c4f8e5429632"
 #elif defined(OS_LINUX)
-#define CEF_API_HASH_PLATFORM "426c0d63f71759f182857d08d7afd0a4108964dd"
+#define CEF_API_HASH_PLATFORM "c8fdbd5a5eea66912e3741daecaebbeba90d552f"
 #endif
 
 ///
