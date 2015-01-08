782b127df2b4e658b2bb2b8d7cceaef0c7b3271c
Sandern <vannoortsander@gmail.com>
2015-01-08 21:14:16 +0100

    Fix mistake in sensing code.

--- src/game/server/lambdawars/unit_sense.h
+++ src/game/server/lambdawars/unit_sense.h
@@ -180,7 +180,7 @@ inline bool UnitBaseSense::HasEnemy( CBaseEntity *pEnemy )
 
 inline bool UnitBaseSense::HasOther( CBaseEntity *pOther )
 {
-	for( int i = 0; i < m_SeenEnemies.Count(); i++ ) 
+	for( int i = 0; i < m_SeenOther.Count(); i++ ) 
 	{
 		if( m_SeenOther.Element(i).entity == pOther )
 			return true;
