e2f0dba9d840dcc7511c2cacafcf4ad7f44615a3
Sandern <vannoortsander@gmail.com>
2015-05-03 19:26:26 +0200

    Fix old tile not removed when new tile is empty (due obstacle covering the tile completely).

--- src/thirdparty/recastnavigation/DetourTileCache/Source/DetourTileCache.cpp
+++ src/thirdparty/recastnavigation/DetourTileCache/Source/DetourTileCache.cpp
@@ -683,8 +683,12 @@ dtStatus dtTileCache::buildNavMeshTile(const dtCompressedTileRef ref, dtNavMesh*
 		return status;
 	
 	// Early out if the mesh tile is empty.
-	if (!bc.lmesh->npolys)
+	if (!bc.lmesh->npolys) {
+		// Remove existing tile.
+		navmesh->removeTile(navmesh->getTileRefAt(tile->header->tx,tile->header->ty,tile->header->tlayer),0,0);
+
 		return DT_SUCCESS;
+	}
 	
 	dtNavMeshCreateParams params;
 	memset(&params, 0, sizeof(params));
